ceeb8c74a8efbf4d4a976a8ef5ba1d88547c9679
UAEDF-tomc <tom.cornelis@ua.ac.be>
2018-04-16 14:42:04 +0200

    Fix rho and effective areas for miniIsolation and relIso, do not call mva's in userfloats to avoid circular dependency

--- python/electronsExtrasTTV_cff.py
+++ python/electronsExtrasTTV_cff.py
@@ -16,32 +16,43 @@ def addTTVIDs(process, options):
     from PhysicsTools.NanoAOD.electrons_cff import slimmedElectronsWithUserData
     from PhysicsTools.NanoAOD.electrons_cff import electronMVATTV2016, electronMVATTV2017
 
-    process.isoForEle = isoForEle 
+    if options['is2016']: effAreas = 'RecoEgamma/ElectronIdentification/data/Spring15/effAreaElectrons_cone03_pfNeuHadronsAndPhotons_25ns.txt'
+    else:                 effAreas = 'RecoEgamma/ElectronIdentification/data/Fall17/effAreaElectrons_cone03_pfNeuHadronsAndPhotons_92X.txt'
+
+    process.isoForEle = isoForEle
+    process.isoForEle.rho_MiniIso    = cms.InputTag("fixedGridRhoFastjetAll")
+    process.isoForEle.EAFile_MiniIso = cms.FileInPath(effAreas)
+    process.isoForEle.EAFile_PFIso   = cms.FileInPath(effAreas)
+
     process.ptRatioRelForEle = ptRatioRelForEle
     process.slimmedElectronsWithUserData = slimmedElectronsWithUserData
-    process.electronMVATTV2016 = electronMVATTV2016
-    process.electronMVATTV2017 = electronMVATTV2017
+
+    process.electronMVATTV = electronMVATTV2016 if options['is2016'] else electronMVATTV2017
 
     # Make a new electron collection, with additional variables that are used for the LeptonMVA below
     process.slimmedElectronsWithUserData.src = cms.InputTag(options['ELECTRON_COLL'])
+    process.slimmedElectronsWithUserData.userFloats = cms.PSet(
+        miniIsoChg = cms.InputTag("isoForEle:miniIsoChg"),
+        miniIsoAll = cms.InputTag("isoForEle:miniIsoAll"),
+        PFIsoChg = cms.InputTag("isoForEle:PFIsoChg"),
+        PFIsoAll = cms.InputTag("isoForEle:PFIsoAll"),
+        ptRatio = cms.InputTag("ptRatioRelForEle:ptRatio"),
+        ptRel = cms.InputTag("ptRatioRelForEle:ptRel"),
+        jetNDauChargedMVASel = cms.InputTag("ptRatioRelForEle:jetNDauChargedMVASel"),
+        )
     process.slimmedElectronsWithUserData.userIntFromBools = cms.PSet() # Removed
     process.slimmedElectronsWithUserData.userInts = cms.PSet() # Removed
 
     # Run the ttH MVA, modify src and take MVA directly from VID (VID must run before this producer)
-    process.electronMVATTV2016.src = cms.InputTag("slimmedElectronsWithUserData")
-    process.electronMVATTV2017.src = cms.InputTag("slimmedElectronsWithUserData")
+    process.electronMVATTV.src = cms.InputTag("slimmedElectronsWithUserData")
 
     # At the end of this, everything we need is either a userfloat or in a producer linked with slimmedElectronsWithUserData
-
-    # Next, we'll call Tom's MyElectronVariableHelper, which will calculate all the needed IDs. Alternatively, we could hack these into the fitter, since all the needed variables exist...
-    # ... if the fitter can edit the probe requirements both at the numerator and the denominator, then all the work can be done there, starting from the loosest Tag/Probe combination!
-
     process.ttvEleVarHelper = cms.EDProducer("TTVElectronVariableHelper",
         probes         = cms.InputTag("slimmedElectronsWithUserData"),
         mvas           = cms.InputTag("electronMVAValueMapProducer:ElectronMVAEstimatorRun2Spring16GeneralPurposeV1Values"),
         dxy            = cms.InputTag("eleVarHelper:dxy"),
         dz             = cms.InputTag("eleVarHelper:dz"),
-        leptonMvas     = cms.InputTag("electronMVATTV2016" if options['is2016'] else "electronMVATTV2017"),
+        leptonMvas     = cms.InputTag("electronMVATTV"),
     )
 
     process.ttv_sequence = cms.Sequence(
